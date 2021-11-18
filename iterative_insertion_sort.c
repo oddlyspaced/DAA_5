8420d39ebd4e6ec61b390c60b38b261459ea3030
oddlyspaced <oddlyspaced1@gmail.com>
2021-11-18 17:35:41 +0530

    Add code for Iterative Insertion Sort

---
+++ iterative_insertion_sort.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void print_arr(int arr[], int size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        printf("%d\t", arr[i]);
+    }
+    printf("\n");
+}
+
+void main() {
+    int arr[] = {3, 5, 4, 7, 2, 8, 10, 1, 11, 0};
+    int size = 10;
+    print_arr(arr, size);
+    int i, j;
+    for (i = 0; i < size; i++) {
+        int v = arr[i];
+        for (j = i + 1; j < size; j++) {
+            int vv = arr[j];
+            if (v > vv) {
+                // swap vv and v
+                int t = arr[i];
+                arr[i] = arr[j];
+                arr[j] = t;                                
+            }
+        }
+    }
+    print_arr(arr, size);
+}
