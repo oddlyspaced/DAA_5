b7732d6e1c62d651c7de9e3146382beb5bde8bf0
oddlyspaced <oddlyspaced1@gmail.com>
2021-12-05 22:47:01 +0530

    Lab2: Refactor Quick sort code

--- Lab2/quick_sort.c
+++ Lab2/quick_sort.c
@@ -1,3 +1,7 @@
+// Quick Sort
+// Hardik Srivastava
+// 199303069
+
 #include<stdio.h>
 
 void print_arr(int arr[], int size) {
@@ -8,42 +12,29 @@ void print_arr(int arr[], int size) {
     printf("\n");
 } 
 
-int partition (int arr[], int low, int high) { 
-    int pivot = arr[high]; // pivot 
-    int i = (low - 1); // Index of smaller element and indicates the right position of pivot found so far
-  
-    for (int j = low; j < high; j++) {
-        // If current element is smaller than the pivot 
+int partition (int arr[], int l, int h) { 
+    int pivot = arr[h];
+    int i = l - 1;
+
+    for (int j = l; j < h; j++) {
         if (arr[j] < pivot) {
-            i++; // increment index of smaller element
-            // swap
+            i++;
             int t = arr[i];
             arr[i] = arr[j];
             arr[j] = t;
         } 
     }
     int t = arr[i + 1];
-    arr[i + 1] = arr[high];
-    arr[high] = t;
-    return (i + 1); 
+    arr[i + 1] = arr[h];
+    arr[h] = t;
+    return i + 1; 
 } 
   
-/* The main function that implements QuickSort 
-arr[] --> Array to be sorted, 
-low --> Starting index, 
-high --> Ending index */
-void quickSort(int arr[], int low, int high) 
-{ 
-    if (low < high) 
-    { 
-        /* pi is partitioning index, arr[p] is now 
-        at right place */
-        int pi = partition(arr, low, high); 
-  
-        // Separately sort elements before 
-        // partition and after partition 
-        quickSort(arr, low, pi - 1); 
-        quickSort(arr, pi + 1, high); 
+void quickSort(int arr[], int l, int h) { 
+    if (l < h) {
+        int pi = partition(arr, l, h); 
+        quickSort(arr, l, pi - 1); 
+        quickSort(arr, pi + 1, h); 
     } 
 } 
 
