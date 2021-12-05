ddf1b65fc7038f473e84acb68b0108032f997dc3
oddlyspaced <oddlyspaced1@gmail.com>
2021-12-05 18:51:32 +0530

    Lab1: Improve Iterative Binary Search code

--- Lab1/iterative_binary_search.c
+++ Lab1/iterative_binary_search.c
@@ -1,26 +1,52 @@
 // Iterative Binary Search
+// Hardik Srivastava
+// 199303069
 
 #include <stdio.h>
-// #include <unistd.h>
 
-void main() {
-    int arr[] = {4, 2, 1, 3, 9, 2, 6, 8, 0, 11};
-    int size = 9;
+int main() {
+    printf("Enter array size: ");
+    int n;
+    scanf("%d", &n);
+
+    printf("Enter array: ");    
+    int arr[n], i;
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
     int low = 0;
-    int high = size;
-    int value_to_find = 9;
-    int mid_index = size / 2;
-    while (arr[mid_index] != value_to_find) {
-        // printf("L: %d M: %d H: %d\n", low, mid_index, high);
+    int high = n - 1;
+
+    printf("Enter value to find: ");
+    int value_to_find;
+    scanf("%d", &value_to_find);
+    
+    int flag = 0;
+    int mid_index;
+
+    while (low < high) {
+        mid_index =  (low + high) / 2;
         int mid_value = arr[mid_index];
+
+        if (mid_value == value_to_find) {
+            flag = 1;
+            break;
+        }
+
         if (value_to_find > mid_value) {
             low = mid_index + 1;
         }
         else if (value_to_find < mid_value) {
-            high = mid_index - 1;
+            high = mid_index;
         }
-        mid_index =  (low + high) / 2;
-        // sleep(1);
     }
-    printf("Value found at: %d", mid_index);
+
+    if (flag == 1) {
+        printf("Value found at: %d", mid_index);
+    }
+    else {
+        printf("Value not found");
+    }
+    return 0;
 }
