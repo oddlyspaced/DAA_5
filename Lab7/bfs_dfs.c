c41720ebeba431f813d02ead06c8cf3f59791366
oddlyspaced <oddlyspaced1@gmail.com>
2021-12-05 22:59:30 +0530

    Lab7: Refactor and update BFS DFS Code

--- Lab7/bfs_dfs.c
+++ Lab7/bfs_dfs.c
@@ -1,35 +1,40 @@
+// BFS DFS
+// Hardik Srivastava
+// 199303069
+
 #include<stdio.h>
 
-#define SIZE 5
+#define V_MAX 99
+int V;
 
-int graph[SIZE][SIZE] = {
-    {0, 1, 0, 1, 0},
-    {0, 0, 1, 0, 1},
-    {0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0},
-    {0, 0, 0, 0, 0},
-};
+int graph[V_MAX][V_MAX];
+
+// int graph[SIZE][SIZE] = {
+//     {0, 1, 0, 1, 0},
+//     {0, 0, 1, 0, 1},
+//     {0, 0, 0, 0, 0},
+//     {0, 0, 0, 0, 0},
+//     {0, 0, 0, 0, 0},
+// };
 
 void dfs(int node) {
     printf("%d\t", node);
     int i;
-    for (i = 0; i < SIZE; i++) {
+    for (i = 0; i < V; i++) {
         if (graph[node][i] == 1) {
-            // printf("%d\t", i);
             dfs(i);
         }
     }
 }
 
 void bfs(int node) {
-    // printf("%d\t", node);
     int i;
-    for (i = 0; i < SIZE; i++) {
+    for (i = 0; i < V; i++) {
         if (graph[node][i] == 1) {
             printf("%d\t", i);
         }
     }
-    for (i = 0; i < SIZE; i++) {
+    for (i = 0; i < V; i++) {
         if (graph[node][i] == 1) {
             bfs(i);
         }
@@ -37,8 +42,21 @@ void bfs(int node) {
 }
 
 int main() {
+    printf("Enter size : ");
+    scanf("%d", &V);
+
+    printf("Enter adjacency matrix : ");
+    int i, j;
+    for (i = 0; i < V; i++) {
+        for (j = 0; j < V; j++) {
+            scanf("%d", &graph[i][j]);
+        }
+    }
+
+    printf("DFS : \n");
     dfs(0);
-    printf("\n");
+
+    printf("\nBFS\n");
     printf("0\t");
     bfs(0);
 }
