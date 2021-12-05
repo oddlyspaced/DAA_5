559b7444004fb4dd112745009ab8bfb34288b450
oddlyspaced <oddlyspaced1@gmail.com>
2021-12-05 22:04:15 +0530

    Lab1: Improve iterative insertion sort

--- Lab1/iterative_insertion_sort.c
+++ Lab1/iterative_insertion_sort.c
@@ -1,3 +1,7 @@
+// Iterative Insertion Sort
+// Hardik Srivastava
+// 199303069
+
 #include<stdio.h>
 
 void print_arr(int arr[], int size) {
@@ -8,19 +12,32 @@ void print_arr(int arr[], int size) {
     printf("\n");
 }
 
-void main() {
-    int arr[] = {3, 5, 4, 7, 2, 8, 10, 1, 11, 0};
-    int size = 10;
-    print_arr(arr, size);
-    int i, j;
+int main() {
+    printf("Enter array size: ");
+    int size;
+    scanf("%d", &size);
+
+    printf("Enter array: ");    
+    int arr[size], i;
     for (i = 0; i < size; i++) {
-        for (j = i + 1; j < size; j++) {
-            if (arr[i] > arr[j]) {
-                int t = arr[i];
-                arr[i] = arr[j];
-                arr[j] = t;                                
-            }
-        }
+        scanf("%d", &arr[i]);
     }
+    
     print_arr(arr, size);
+    int j, k;
+
+    for (i = 1; i < size; i++) {
+        k = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > k) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = k;
+    }
+
+    printf("Sorted Array:\n");
+    print_arr(arr, size);
+    return 0;
 }
