2762e132a0f769e26c7e8aa0128949b1381b94bb
oddlyspaced <oddlyspaced1@gmail.com>
2021-12-05 22:55:01 +0530

    Lab6: Refactor and update Prims code

--- Lab6/prims.c
+++ Lab6/prims.c
@@ -1,62 +1,69 @@
-#include<stdio.h>
+// Prims Algo
+// Hardik Srivastava
+// 199303069
 
+#include <stdio.h>
 #define MAX 9999
+#define V_MAX 99
+int V;
 
-// int G[V][V] = {
-//   {0, 9, 75, 0, 0},
-//   {9, 0, 95, 19, 42},
-//   {75, 95, 0, 51, 66},
-//   {0, 19, 51, 0, 31},
-//   {0, 42, 66, 31, 0}};
+int mincost(int cost[], int visited[]) {
+	int min = MAX, index, i;
+	for(i = 0; i < V; i++) {
+		if(visited[i] == 0 && cost[i] < min) {
+			min = cost[i];
+			index = i;
+		}
+	}
+	return index;
+}
+
+void display(int parent[], int graph[V_MAX][V_MAX]) {
+	int i, j, min;
+	printf("Edge\t Weight\n");
+	for(i = 1; i < V; i++) {
+		min = 1;
+		for(j = 1; j < V; j++) {
+			if(graph[min][parent[min]] > graph[j][parent[j]]) {
+				min = j;
+			}
+		}
+		printf("%d - %d\t %d\n", parent[min], min, graph[min][parent[min]]);
+		graph[min][parent[min]] = MAX;
+	}
+}
+
+void prims(int Graph[V_MAX][V_MAX]) {
+	int parent[V], cost[V], visited[V], start, i,j;
+	for(i = 0 ; i < V; i++) {
+		cost[i] = 9999;
+		visited[i] = 0;
+	}
+	cost[0] = 0;
+	parent[0] = -1;
+	for(i = 0; i < V-1; i++) {
+		start = mincost(cost,visited);
+		visited[start] = 1;
+		for(j = 0; j < V; j++) {
+			if(Graph[start][j] != 0 && visited[j] == 0 && Graph[start][j] < cost[j]) {
+				parent[j] = start;
+				cost[j] = Graph[start][j];
+			}
+		}
+	}
+	display(parent, Graph);
+}
 
 int main() {
-    int V;
-    scanf("%d", &V);
-
-    int G[V][V];
-    int i, j;
-    for (i = 0; i < V; i++) {
-        for (j = 0; j < V; j++) {
-            scanf("%d", &G[i][j]);
-        }
-    }
-
-
-    int n;
-    int selected[V];
-    
-    for (i = 0; i < V; i++) {
-        selected[i] = 0;
-    }
-    
-    // current edge as 0 (starting)
-    n = 0;
-    selected[0] = 1;
-    
-    int x, y;
-    // one less than total
-    while (n < V - 1) {
-        int min = MAX;
-        x = 0;
-        y = 0;
-        
-        for (int i = 0; i < V; i++) {
-            if (selected[i]) {
-                for (int j = 0; j < V; j++) {
-                    if (!selected[j] && G[i][j]) {  // not in selected and there is an edge
-                    if (min > G[i][j]) {
-                        min = G[i][j];
-                        x = i;
-                        y = j;
-                    }
-                }
-            }
-        }
-    }
-    printf("%d - %d : %d\n", x, y, G[x][y]);
-    selected[y] = 1;
-    n++;
-  }
-
-  return 0;
+	printf("Enter size:\n");
+	scanf("%d", &V);
+	int graph[V_MAX][V_MAX];
+	int i, j;
+	for (i = 0; i < V; i++) {
+		for (j = 0; j < V; j++) {
+			scanf("%d", &graph[i][j]);
+		}
+	}
+	prims(graph);
+	return 0;
 }
